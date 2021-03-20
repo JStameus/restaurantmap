adab00521a185a8ef4f606e059945fbad40a70c8
JStameus <joseph.stameus@gmail.com>
2021-03-20 20:29:27 +0100

    Added card.js with a class that will (hopefully) help replace all the chunky code for rendering restaurant cards. Added it in the index.html file as well

---
+++ card.js
@@ -0,0 +1,17 @@
+class RestaurantCard {
+    constructor(name, street, phone, website) {
+        this.name = name;
+        this.street = street;
+        this.phone = phone;
+        this.website = website;
+    }
+
+    render() {
+        return `<div class="restaurantcard">
+                    <h2>${this.name}</h2> 
+                    <h3>${this.street}</h3>
+                    <p>${this.phone}</p>
+                    <a href="${this.website}">Website</a>
+                </div>`
+    }
+} 
